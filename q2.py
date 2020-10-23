7f1e1f38e9d46537e328a60e8913c647477ac2ea
Akashdeep <akashdeepsingh98@gmail.com>
2020-10-23 17:20:08 +0530

    q2 seemingly done

--- q2.py
+++ q2.py
@@ -14,6 +14,8 @@ months = {
     'dec': 12
 }
 
+days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
 separators = ['-', '/', '.']
 
 
@@ -26,9 +28,28 @@ def prepare(date):
         return [day, month, year]
 
     else:
-        temp = date[0].split('/')
-        print(temp)
-    return date
+        for sep in separators:
+            temp = date[0].split(sep)
+            if len(temp) == 3:
+                temp = list(map(int, temp))
+                break
+        return temp
+
+
+def distance(date):
+    numdays = 0
+    if date[1] > 2:
+        numleap = date[2]//4 - date[2]//100 + date[2]//400
+    else:
+        numleap = (date[2]-1)//4 - (date[2]-1)//100 + (date[2]-1)//400
+
+    numdays = date[2]*365 + numleap
+    # num of days in months before
+    for m in range(1, date[1]):
+        numdays+=days[m-1]
+    numdays+=date[0]
+
+    return numdays
 
 
 date1 = []
@@ -44,3 +65,11 @@ date2 = date2[1:]
 
 date1 = prepare(date1)
 date2 = prepare(date2)
+#print(date1)
+#print(date2)
+dist1 = distance(date1)
+dist2 = distance(date2)
+print(dist1)
+print(dist2)
+
+print(abs(dist1-dist2))
