053e42f63bf24ba7b305aef2a8ca6524e0ca5ff8
Akashdeep <akashdeepsingh98@gmail.com>
2020-10-24 11:05:29 +0530

    giving free slots

--- q3.py
+++ q3.py
@@ -44,19 +44,112 @@ def getFreeTime(emp, dur):
                 start = datetime.combine(key, interval[1])
                 continue
             else:
-                length = (end-start).min
-                if length >= timedelta(minutes=dur):
+                length = (datetime.combine(key, interval[0])-start).seconds//60
+                if length >= dur:
                     slotstart = start
-                    while slotstart + timedelta(minutes=dur) < end:
+                    while slotstart + timedelta(minutes=dur) <= datetime.combine(key, interval[0]):
                         slots.append(
-                            [slotstart, slotstart+timedelta(minutes=dur)])
+                            [slotstart.time(), (slotstart+timedelta(minutes=dur)).time()])
                         slotstart = slotstart + timedelta(minutes=dur)
-                    #slots.append([start, interval[0]])
             start = datetime.combine(key, interval[1])
         if start != end:
-            slots.append([start, end])
+            length = (end-start).seconds//60
+            if length >= dur:
+                slotstart = start
+                while slotstart + timedelta(minutes=dur) <= end:
+                    slots.append(
+                        [slotstart.time(), (slotstart+timedelta(minutes=dur)).time()])
+                    slotstart = slotstart + timedelta(minutes=dur)
         emp[key] = slots
     return emp
+
+
+def matchFreeUtil(emp1, emp2, dur):
+    numSlots = dur//30
+    done = 0
+    result = {}
+    for day in emp1.keys():
+        i = 0
+        j = 0
+        while i < len(emp1[day]) and j < len(emp2[day]):
+            if datetime.combine(day, emp1[day][i][0]) > datetime.combine(day, emp2[day][j][0]):
+                j += 1
+            elif datetime.combine(day, emp1[day][i][0]) < datetime.combine(day, emp2[day][j][0]):
+                i += 1
+            else:
+                done += 1
+                if day not in result.keys():
+                    result[day] = []
+                if numSlots == 1:
+                    result[day].append(emp1[day][i])
+                    return result
+                else:
+                    if len(result[day]) == 0:
+                        result[day].append(emp1[day][i])
+                    else:
+                        if result[day][-1][1] == emp1[day][i][0]:
+                            result[day].append(emp1[day][i])
+                        else:
+                            done = 0
+                            result[day] = []
+                if done == numSlots:
+                    return result
+                i+=1
+                j+=1
+        while i < len(emp1[day]):
+            if datetime.combine(day, emp1[day][i][0]) < datetime.combine(day, emp2[day][j][0]):
+                i += 1
+            else:
+                done += 1
+                if day not in result.keys():
+                    result[day] = []
+                if numSlots == 1:
+                    result[day].append(emp1[day][i])
+                    return result
+                else:
+                    if len(result[day]) == 0:
+                        result[day].append(emp1[day][i])
+                    else:
+                        if result[day][-1][1] == emp1[day][i][0]:
+                            result[day].append(emp1[day][i])
+                        else:
+                            done = 0
+                            result[day] = []
+                if done == numSlots:
+                    return result
+                i+=1
+        while j < len(emp2[day]):
+            if datetime.combine(day, emp1[day][i][0]) > datetime.combine(day, emp2[day][j][0]):
+                j += 1
+            else:
+                done += 1
+                if day not in result.keys():
+                    result[day] = []
+                if numSlots == 1:
+                    result[day].append(emp1[day][i])
+                    return result
+                else:
+                    if len(result[day]) == 0:
+                        result[day].append(emp1[day][i])
+                    else:
+                        if result[day][-1][1] == emp1[day][i][0]:
+                            result[day].append(emp1[day][i])
+                        else:
+                            done = 0
+                            result[day] = []
+                if done == numSlots:
+                    return result
+                j+=1
+    return result
+
+
+def matchFree(emp1, emp2, dur):
+    result = matchFreeUtil(emp1, emp2, dur)
+    for day in result.keys():
+        start = result[day][0][0]
+        end = result[day][-1][1]
+        result[day] = [start, end]
+    return result
 
 
 emp1 = {}
@@ -79,13 +172,16 @@ emp2 = emp2['Employee2']
 emp1 = preprocess(emp1)
 emp2 = preprocess(emp2)
 
-print(emp1)
-print(emp2)
+#print(emp1)
+#print(emp2)
 
 emp1, emp2 = remUnnDat(emp1, emp2)
 
-emp1 = getFreeTime(emp1, dur)
-emp2 = getFreeTime(emp2, dur)
+emp1 = getFreeTime(emp1, 30)
+emp2 = getFreeTime(emp2, 30)
 
 print(emp1)
 print(emp2)
+
+print('result is ')
+print(matchFree(emp1, emp2, dur))
